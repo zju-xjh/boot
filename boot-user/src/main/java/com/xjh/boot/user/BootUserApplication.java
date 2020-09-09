37a4e35930508518ecaff729631941e85fd133a5
zju-xjh <1252736593@qq.com>
2020-09-09 11:36:27 +0800

    add

---
+++ boot-user/src/main/java/com/xjh/boot/user/BootUserApplication.java
@@ -0,0 +1,13 @@
+package com.xjh.boot.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUserApplication.class, args);
+    }
+
+}
---
+++ boot-user/src/test/java/com/xjh/boot/user/BootUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjh.boot.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
